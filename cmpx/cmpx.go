9e594e781c55eb9f2ed1caef2a17e0b6f01bcc8b
Daniel Francesconi <daniel.francesconi@hgv.it>
2024-06-28 08:21:49 +0200

    Prefer using the built-in `Compare` function from the time package

--- cmpx/cmpx.go
+++ cmpx/cmpx.go
@@ -1,11 +1,5 @@
 package cmpx
 
-import "time"
-
-func Time(a, b time.Time) int {
-	return int(a.Sub(b))
-}
-
 func Bool[T ~bool](a, b T) int {
 	if !a && b {
 		return -1
--- cmpx/cmpx_test.go
+++ cmpx/cmpx_test.go
@@ -2,45 +2,9 @@ package cmpx
 
 import (
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
 )
-
-func TestTime(t *testing.T) {
-	tests := []struct {
-		a, b     time.Time
-		expected func(int) bool
-	}{
-		{
-			a:        time.Date(2024, 6, 1, 12, 0, 0, 0, time.UTC),
-			b:        time.Date(2024, 6, 1, 11, 0, 0, 0, time.UTC),
-			expected: func(i int) bool { return i > 0 },
-		},
-		{
-			a:        time.Date(2024, 6, 1, 12, 0, 0, 0, time.UTC),
-			b:        time.Date(2024, 6, 1, 12, 0, 0, 0, time.UTC),
-			expected: func(i int) bool { return i == 0 },
-		},
-		{
-			a:        time.Date(2024, 6, 1, 11, 0, 0, 0, time.UTC),
-			b:        time.Date(2024, 6, 1, 12, 0, 0, 0, time.UTC),
-			expected: func(i int) bool { return i < 0 },
-		},
-		{
-			a:        time.Date(2024, 6, 1, 12, 0, 0, 0, time.UTC),
-			b:        time.Date(2024, 6, 1, 11, 59, 0, 0, time.UTC),
-			expected: func(i int) bool { return i > 0 },
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run("", func(t *testing.T) {
-			result := Time(tt.a, tt.b)
-			assert.True(t, tt.expected(result))
-		})
-	}
-}
 
 func TestBool(t *testing.T) {
 	tests := []struct {

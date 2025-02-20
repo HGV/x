a9ab1eda4c3ec8377c292bc43169e7d05e031666
David Mair Spiess <david.spiess@hgv.it>
2025-02-20 16:12:33 +0100

    Add abort handler test

--- middlewarex/recoverer_test.go
+++ middlewarex/recoverer_test.go
@@ -8,7 +8,25 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestRecoverer_WithPlainTextError(t *testing.T) {
+func TestRecovererAbortHandler(t *testing.T) {
+	defer func() {
+		rcv := recover()
+		if rcv != http.ErrAbortHandler {
+			t.Fatalf("http.ErrAbortHandler should not be recovered")
+		}
+	}()
+
+	next := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		panic(http.ErrAbortHandler)
+	})
+
+	h := Recoverer(nil)(next)
+	r := httptest.NewRequest(http.MethodGet, "/", nil)
+	w := httptest.NewRecorder()
+	h.ServeHTTP(w, r)
+}
+
+func TestRecovererCustomErrorResponse(t *testing.T) {
 	plainTextErrorHandler := func(w http.ResponseWriter, r *http.Request, err error) {
 		w.WriteHeader(http.StatusInternalServerError)
 		_, _ = w.Write([]byte("internal server error: " + err.Error()))

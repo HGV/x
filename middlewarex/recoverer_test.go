59c08fa3ed9470758b2c5905b27dea96c1571b69
David Mair Spiess <david.spiess@hgv.it>
2025-02-20 10:28:59 +0100

    Remove JSON error test

--- middlewarex/recoverer_test.go
+++ middlewarex/recoverer_test.go
@@ -1,7 +1,6 @@
 package middlewarex
 
 import (
-	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -27,36 +26,4 @@ func TestRecoverer_WithPlainTextError(t *testing.T) {
 	res := w.Result()
 	assert.Equal(t, res.StatusCode, http.StatusInternalServerError)
 	assert.Equal(t, w.Body.String(), "internal server error: test panic")
-}
-
-func TestRecoverer_WithJSONError(t *testing.T) {
-	type ErrorResponse struct {
-		Status  int    `json:"status"`
-		Message string `json:"message"`
-	}
-
-	jsonErrorHandler := func(w http.ResponseWriter, r *http.Request, err error) {
-		w.WriteHeader(http.StatusInternalServerError)
-		_ = json.NewEncoder(w).Encode(ErrorResponse{
-			Status:  http.StatusInternalServerError,
-			Message: err.Error(),
-		})
-	}
-
-	next := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		panic("test panic")
-	})
-
-	h := Recoverer(jsonErrorHandler)(next)
-	r := httptest.NewRequest(http.MethodGet, "/", nil)
-	w := httptest.NewRecorder()
-	h.ServeHTTP(w, r)
-
-	res := w.Result()
-	var responseBody ErrorResponse
-	_ = json.NewDecoder(res.Body).Decode(&responseBody)
-
-	assert.Equal(t, res.StatusCode, http.StatusInternalServerError)
-	assert.Equal(t, http.StatusInternalServerError, responseBody.Status)
-	assert.Equal(t, "test panic", responseBody.Message)
 }

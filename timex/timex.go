1520c7bec40310e106712821549b27cdd98b5d21
Daniel Francesconi <daniel.francesconi@hgv.it>
2025-06-12 12:26:51 +0200

    feat: add `MakeDateSeries` to generate a date range given an interval

--- timex/timex.go
+++ timex/timex.go
@@ -8,7 +8,12 @@ const (
 	Day time.Duration = 24 * time.Hour
 )
 
+// Deprecated: timex.GenerateSeries is deprecated. Use timex.MakeTimeSeries instead.
 func GenerateSeries(start, stop time.Time, interval time.Duration) []time.Time {
+	return MakeTimeSeries(start, stop, interval)
+}
+
+func MakeTimeSeries(start, stop time.Time, interval time.Duration) []time.Time {
 	if start.After(stop) || interval <= 0 {
 		return nil
 	}
@@ -17,6 +22,18 @@ func GenerateSeries(start, stop time.Time, interval time.Duration) []time.Time {
 	series := make([]time.Time, 0, l)
 	for t := start; !t.After(stop); t = t.Add(interval) {
 		series = append(series, t)
+	}
+	return series
+}
+
+func MakeDateSeries(start, stop Date, intervalDays int) []Date {
+	if start.After(stop) || intervalDays <= 0 {
+		return nil
+	}
+
+	series := make([]Date, 0, stop.DaysSince(start))
+	for d := start; !d.After(stop); d = d.AddDays(intervalDays) {
+		series = append(series, d)
 	}
 	return series
 }
--- timex/timex_test.go
+++ timex/timex_test.go
@@ -8,21 +8,42 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGenerateSeries(t *testing.T) {
+func TestMakeTimeSeries(t *testing.T) {
 	start := time.Now()
 	stop := start.AddDate(0, 0, 9)
 
 	t.Run("should return nil", func(t *testing.T) {
-		assert.Nil(t, GenerateSeries(start, stop, 0))
-		assert.Nil(t, GenerateSeries(stop, start, Day))
+		assert.Nil(t, MakeTimeSeries(start, stop, 0))
+		assert.Nil(t, MakeTimeSeries(stop, start, Day))
 	})
 
 	t.Run("should return a series of dates", func(t *testing.T) {
-		actual := GenerateSeries(start, start.AddDate(0, 0, 9), Day)
+		actual := MakeTimeSeries(start, start.AddDate(0, 0, 9), Day)
 		expected := make([]time.Time, 10)
 		for i := range expected {
 			expected[i] = start.AddDate(0, 0, i)
 		}
 		assert.True(t, slices.EqualFunc(actual, expected, time.Time.Equal))
+	})
+}
+
+func TestMakeDateSeries(t *testing.T) {
+	start := Today()
+	stop := start.AddDays(9)
+
+	t.Run("should return nil", func(t *testing.T) {
+		assert.Nil(t, MakeDateSeries(start, stop, 0))
+		assert.Nil(t, MakeDateSeries(stop, start, 1))
+	})
+
+	t.Run("should return a series of dates", func(t *testing.T) {
+		actual := MakeDateSeries(start, start.AddDays(9), 1)
+		expected := make([]Date, 10)
+		for i := range expected {
+			expected[i] = start.AddDays(i)
+		}
+		assert.True(t, slices.EqualFunc(actual, expected, func(a, b Date) bool {
+			return a.Compare(b) == 0
+		}))
 	})
 }

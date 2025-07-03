0be8993fb090f1aef39a2c595f5512b294751ae2
Daniel Francesconi <daniel.francesconi@hgv.it>
2025-07-03 15:58:44 +0200

    feat: add NullDateRange

--- timex/date_range.go
+++ timex/date_range.go
@@ -43,3 +43,38 @@ func (d DateRange) Bounds() (lower, upper any) {
 
 var _ pgtype.RangeScanner = &DateRange{}
 var _ pgtype.RangeValuer = DateRange{}
+
+type NullDateRange struct {
+	DateRange DateRange
+	Valid     bool
+}
+
+func (nd *NullDateRange) ScanNull() error {
+	*nd = NullDateRange{}
+	return nil
+}
+
+func (nd *NullDateRange) ScanBounds() (lowerTarget, upperTarget any) {
+	return nd.DateRange.ScanBounds()
+}
+
+func (nd *NullDateRange) SetBoundTypes(lower, upper pgtype.BoundType) error {
+	nd.DateRange.SetBoundTypes(lower, upper)
+	nd.Valid = true
+	return nil
+}
+
+func (nd NullDateRange) IsNull() bool {
+	return !nd.Valid
+}
+
+func (nd NullDateRange) BoundTypes() (lower, upper pgtype.BoundType) {
+	return nd.DateRange.BoundTypes()
+}
+
+func (nd NullDateRange) Bounds() (lower, upper any) {
+	return nd.DateRange.Bounds()
+}
+
+var _ pgtype.RangeScanner = &NullDateRange{}
+var _ pgtype.RangeValuer = NullDateRange{}
